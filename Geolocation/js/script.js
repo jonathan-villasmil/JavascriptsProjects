b196538ebecb15005b9bac54f881bee31b8146b2
jonathan <jonathanvillasmildeabreu@gmail.com>
2023-10-02 14:41:18 +0200

    Geolocation

---
+++ Geolocation/js/script.js
@@ -0,0 +1,12 @@
+const arrow =document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data)=>{
+    //console.log(data);
+    // speed.textContent = Math.round(data.coords.speed);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err)=>{
+    console.err(err);
+    alert('HEY! YOU GOTTA ALLOW THAT TO HAPPEN!!!!');
+});
